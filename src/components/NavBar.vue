008c8d99b2a96f9d2791b7e8acf32652da8e9b58
THOMAS MAIRE <thomas.maire@cheops.fr>
2020-07-13 09:24:19 +0200

    Modification navbar + dossier dist

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -18,10 +18,10 @@
                         <router-link :to="{ name: 'NonAlcoholicDrink' }" class="nav-link">Sans alcool</router-link>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                <!-- <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                </form> -->
             </div>
         </nav>
     </div>
